05735d89f0980baf48a2c31af9469fcedb138097
ffalconet <f_falconet@yahoo.fr>
2023-09-08 16:36:22 +0200

    Push Falco Keys

--- src/tools/constants.js
+++ src/tools/constants.js
@@ -1,9 +1,9 @@
 module.exports = {
 	ADMIN_ROLE: 'admin',
-	CKO_SECRET_KEY: 'sk_sbox_2omkl6ihomrvhfg42lq6rur7hiv',
-	CKO_PUBLIC_KEY: 'pk_sbox_pcgilmzxdmboupxbqiuwmixdha=',
-	//CKO_SECRET_KEY: 'sk_sbox_ilnc6s3z5uop45uatcgu2ldfler',
-	//CKO_PUBLIC_KEY: 'pk_sbox_w2jd5htaawrbo4eeziyn2dmifeu',
+	//CKO_SECRET_KEY: 'sk_sbox_2omkl6ihomrvhfg42lq6rur7hiv', // Blablacar
+	//CKO_PUBLIC_KEY: 'pk_sbox_pcgilmzxdmboupxbqiuwmixdha=', // Blablacar
+	CKO_SECRET_KEY: 'sk_sbox_ilnc6s3z5uop45uatcgu2ldfler',
+	CKO_PUBLIC_KEY: 'pk_sbox_w2jd5htaawrbo4eeziyn2dmifeu',
 	//CKO_SECRET_KEY: 'sk_sbox_ppw4amgsrv3425uvgdy4nx2uom6',
 	//CKO_PUBLIC_KEY: 'pk_sbox_7r34xejrkepaa7muecjdejxlda4',
 	IMAGEKIT_PUBLIC_KEY: 'public_M4O+PrfIqziXq+4C8rJrCKa9CRg=',
