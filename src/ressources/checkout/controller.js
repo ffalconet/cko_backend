dc87a72a642a772b59b9f8763ca21a028b2d0083
ffalconet <76886249+ffalconet@users.noreply.github.com>
2024-05-16 09:59:36 +0200

    send to Webhook

--- src/ressources/checkout/controller.js
+++ src/ressources/checkout/controller.js
@@ -342,6 +342,9 @@ exports.validateAppleSession = async (req, res) => {
 	  });
   
 	  console.log("Apple Pay payment outcome", payment);
+	  await axios.post('https://webhook.site/d28cc1d9-8ffe-49a1-9fb8-15e242e8c6d8',
+			payment);
+	
 	  res.send(payment);
 	} catch (err) {
 	  res.status(500).send(err);
